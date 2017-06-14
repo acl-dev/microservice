aeb1b16431506dc55394d93dac92e18966798a08
fuwangqin <382018309@qq.com>
2017-06-14 22:36:26 +0800

    add codes

--- http_rpc/src/http_rpc_config.cpp
+++ http_rpc/src/http_rpc_config.cpp
@@ -4,7 +4,7 @@ namespace acl
 {
 	namespace http_rpc_config
 	{
-		//配置定义
+
 		int var_cfg_redis_conn_timeout = 5;
 		int var_cfg_redis_rw_timeout =30;
 		int var_cfg_rpc_conn_check_inter = 1;
@@ -14,17 +14,17 @@ namespace acl
 		int var_cfg_rw_timeout = 30;
 
 		char *var_cfg_redis_addr;
-		char *var_cfg_memcache_addr = "127.0.0.1:11211";
-		char *var_cfg_allow_clients = "all";
-		char *var_cfg_find_service = "/nameserver/service_mgr/find";
-		char *var_cfg_find_services = "/nameserver/service_mgr/finds";
-		char *var_cfg_add_service = "/nameserver/service_mgr/add";
-		char *var_cfg_del_service = "/nameserver/service_mgr/del";
-		char *var_cfg_list_services = "/nameserver/service_mgr/list";
-		char *var_cfg_server_addr;//需要自己配置
-		char *var_cfg_server_name;//需要自己配置
-		char *var_cfg_nameserver = "servername";
-		char *var_cfg_nameserver_addr = "127.0.0.1:10080";//需要自己配置
+		char *var_cfg_memcache_addr = (char*)"127.0.0.1:11211";
+		char *var_cfg_allow_clients = (char*)"all";
+		char *var_cfg_find_service = (char*)"/nameserver/service_mgr/find";
+		char *var_cfg_find_services = (char*)"/nameserver/service_mgr/finds";
+		char *var_cfg_add_service = (char*)"/nameserver/service_mgr/add";
+		char *var_cfg_del_service = (char*)"/nameserver/service_mgr/del";
+		char *var_cfg_list_services = (char*)"/nameserver/service_mgr/list";
+		char *var_cfg_server_addr;
+		char *var_cfg_server_name;
+		char *var_cfg_nameserver = (char*)"servername";
+		char *var_cfg_nameserver_addr = (char*)"127.0.0.1:10080";
 
 		int var_cfg_sync_del_service_addr = 1;
 		int var_cfg_sync_del_nameserver_service = 0;
